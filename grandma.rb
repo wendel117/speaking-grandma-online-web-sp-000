1d45d79393055c1b012a97a5b1a7c305797a4280
Wendy Hamilton <wdhamilton117@gmail.com>
2019-03-08 21:46:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,9 @@
 
 def speak_to_grandma(input)
   if input == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN"
+    return "I LOVE YOU TOO PUMPKIN"
   else
-    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   end
 end
 
