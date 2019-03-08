9d317cba5f51231f18e2649ac932f57080a0bf66
Wendy Hamilton <wdhamilton117@gmail.com>
2019-03-08 21:46:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,7 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(input)
-  if input == input.upcase
-    puts "HUH?! SPEAK UP, SONNY!"
-  elsif input == "I LOVE YOU GRANDMA!"
+  if input == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN"
   else
     puts "NO, NOT SINCE 1938!"
